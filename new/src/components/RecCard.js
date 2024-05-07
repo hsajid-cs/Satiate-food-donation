e09e3667050a2b8bac30bd1599368d269e3e62b1
Hira Sajid <151638454+notmakhan@users.noreply.github.com>
2024-05-07 20:34:21 +0500

    reccard

--- new/src/components/RecCard.js
+++ new/src/components/RecCard.js
@@ -24,7 +24,7 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function TestCard() {
+export default function RecCard() {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
