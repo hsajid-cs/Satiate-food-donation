f7907ad2b63928d4fcd622b0177f6a4371e491fe
Hira Sajid <151638454+notmakhan@users.noreply.github.com>
2024-05-08 21:41:11 +0500

    rider updates status

---
+++ new/src/components/UpdateStatus.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { MenuItem } from '@mui/material';
+
+const status = [
+    { value: 'Picked', label: 'Picked' },
+    { value: 'Delivered', label: 'Delivered' },
+
+];
+export default function UpdateStatus() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Update Status
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>List New Food</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            select
+            margin="dense"
+            id="contact"
+            name="contact"
+            type="number"
+            helperText="Serving Size (Min. 20)"
+            fullWidth
+            variant="outlined">{status.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}</TextField>
+            
+  
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit" >Add</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
